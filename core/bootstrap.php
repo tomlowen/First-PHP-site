5d55095cb423a0716211ae777da646a0e5c5e94a
tomlowen <tom.r.lowen@googlemail.com>
2021-12-22 12:00:52 +0000

    lesson 20

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,6 +1,14 @@
 <?php
+
+$app = [];
+
+$app['config'] = require 'config.php';
+
+require 'core/Router.php';
+require 'core/Request.php';
 require 'core/database/Connection.php';
 require 'core/database/QueryBuilder.php';
-$config = require 'config.php';
 
-return new QueryBuilder(Connection::make($config['database']));
+$app['database'] = new QueryBuilder(
+    Connection::make($app['config']['database'])
+);
